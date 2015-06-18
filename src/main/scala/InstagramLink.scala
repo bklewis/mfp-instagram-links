eab643642ad4dc9238b25b4af997727657afefbc
Bryce Lewis <blewis@myfitnesspal.com>
2015-06-18 11:07:16 -0700

    Added .gitignore, case class InstagramLink

---
+++ src/main/scala/InstagramLink.scala
@@ -0,0 +1,9 @@
+/**
+ * Created by brycelewis on 6/18/15.
+ */
+case class InstagramLink (linkId: String,
+                         url: String,
+                         hashtag: String,
+                         ig_username: String,
+                         ig_postdate: String,
+                         created_at: String)
